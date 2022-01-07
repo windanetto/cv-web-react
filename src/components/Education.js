f20c09f2a0810446a74bad621fe7513e554c0544
windaAgusthia <winda.agusthia@codify.id>
2022-01-07 17:33:48 +0700

    implement UI education and skills page

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,9 +1,21 @@
-import { Flex, Text } from "@chakra-ui/react"
+import { Flex, Text, Box, Spacer } from "@chakra-ui/react"
 
 export const Education = () => {
   return (
-    <Flex>
-      <Text>Education</Text>
+    <Flex w='75%' flexDir='column' justifyContent='center' padding='8vh'>
+      <Flex w='100%' flexDir='column'>
+        <Text fontSize='6xl' fontWeight='bold' mb='3vh'>EDUCATION</Text>
+      </Flex>
+      <Flex w='100%'>
+        <Box fontSize='3xl' fontWeight='bold'>Bachelor of Computer Science</Box>
+        <Spacer/>
+        <Box fontSize='lg' fontWeight='bold' color='#ce8665' pt={17}>2014 - 2019</Box>
+      </Flex>
+      <Flex w='100%' flexDir='column'>
+        <Text fontSize='2xl' mb='12px' fontWeight='semibold'>Sriwijaya University</Text>
+        <Text fontSize='xl'>Informatics Engineering</Text>
+        <Text fontSize='xl'>GPA 3.48/4.00 with 149 credits</Text>
+      </Flex>
     </Flex>
   )
 }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,11 +1,50 @@
-import { Flex, Text } from "@chakra-ui/react"
+import { Flex, Text, Link } from "@chakra-ui/react";
+import { 
+  FaAngular, 
+  FaHtml5, 
+  FaCss3Alt, 
+  FaBootstrap,
+  FaReact,
+  FaPhp,
+  FaFileWord,
+  FaFileExcel,
+  FaFilePowerpoint
+} from 'react-icons/fa';
+import { IoLogoIonic } from 'react-icons/io5'
+import { SiCodeigniter } from 'react-icons/si'
 
 export const Skills = () => {
   return (
-    <Flex>
-      <Text>Skills</Text>
+    <Flex w='75%' flexDir='column' justifyContent='center' padding='8vh'>
+      <Flex w='100%' flexDir='column'>
+        <Text fontSize='6xl' fontWeight='bold' mb='3vh'>SKILLS</Text>
+      </Flex>
+      <Flex w='100%'>
+        <Text fontSize='3xl' fontWeight='bold'>PROGRAMMING LANGUAGES & TOOLS</Text>
+      </Flex>
+      <Flex w='100%' flexDir='column' display='inline' mt={30}>
+        <FaAngular style={styles.iconSosmed}/> 
+        <FaHtml5 style={styles.iconSosmed}/> 
+        <FaCss3Alt style={styles.iconSosmed}/> 
+        <FaBootstrap style={styles.iconSosmed}/> 
+        <IoLogoIonic style={styles.iconSosmed}/> 
+        <FaReact style={styles.iconSosmed}/> 
+        <FaPhp style={styles.iconSosmed}/> 
+        <SiCodeigniter style={styles.iconSosmed}/> 
+        <FaFileWord style={styles.iconSosmed}/> 
+        <FaFileExcel style={styles.iconSosmed}/> 
+        <FaFilePowerpoint style={styles.iconSosmed}/> 
+      </Flex>
     </Flex>
   )
 }
 
 export default Skills
+
+const styles = {
+  iconSosmed: {
+    fontSize: '60px',
+    display: 'inline',
+    marginRight: '5px'
+  }
+}
